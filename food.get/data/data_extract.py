79900c33e2f4eb133a5ab512e60108eb5f6e6169
Danielle Rosenthal <daniellerosenthal@Danielles-MacBook-Air.local>
2024-02-06 14:31:20 -0600

    adding data_extract

--- food.get/data/data_extract.py
+++ food.get/data/data_extract.py
@@ -63,3 +63,53 @@ def import_snap_retailers_data():
 
     return snap_retailer_data
 
+import requests
+import time
+
+census_base_call = 'http://api.census.gov/data/2022/acs/acs5'
+CENSUS_API_KEY = '33d3a1aa6c54c6ef7e892aba406cc5fbb1c743fa'
+params = {'key': CENSUS_API_KEY}
+cbc = 'https://api.census.gov/data/2022/acs/acs5?get=NAME,B01001_001E&for=tract:*&in=state:17&key=33d3a1aa6c54c6ef7e892aba406cc5fbb1c743fa'
+
+# Columns related to features of the census tract itself at a high level
+tract_identifying_columns = ['CensusTract', 'State', 'County', 'Urban', 
+                    'Pop2010', 'OHU2010', 'GroupQuartersFlag', 'NUMGQTRS', 'PCTGQTRS',
+ 'LowIncomeTracts', 'PovertyRate', 'MedianFamily Income', 'LATracts_half', 'LATracts1', 'LATracts10', 'LATracts20', 'TractSNAP', 'TractLOWI']
+
+tract_half_mile = ['lapophalf','lapophalfshare','lalowihalf','lalowihalfshare','lakidshalf','lakidshalfshare','laseniorshalf','laseniorshalfshare','lawhitehalf','lawhitehalfshare','lablackhalf','lablackhalfshare','laasianhalf','laasianhalfshare','lanhopihalf','lanhopihalfshare','laaianhalf','laaianhalfshare','laomultirhalf','laomultirhalfshare','lahisphalf','lahisphalfshare','lahunvhalf','lahunvhalfshare','lasnaphalf','lasnaphalfshare']
+
+tract_one_mile = ['lapop1','lapop1share','lalowi1','lalowi1share','lakids1','lakids1share','laseniors1','laseniors1share','lawhite1','lawhite1share','lablack1','lablack1share','laasian1','laasian1share','lanhopi1','lanhopi1share','laaian1','laaian1share','laomultir1','laomultir1share','lahisp1','lahisp1share','lahunv1','lahunv1share','lasnap1','lasnap1share']
+
+relevant_columns = ['CensusTract', 'State', 'County', 'Urban', 
+                    'Pop2010', 'OHU2010', 'lapop1',
+                    'lapop1share', 'lalowi1', 'lalowi1share', 'lasnap1', 'lasnap1share', 
+                    'lapop10',
+                    'lapop10share', 'lalowi10', 'lalowi10share', 
+                    'lapop20', 'lapop20share', 'lalowi20',
+'lalowi20share']
+
+# get rid of long distance; 
+
+#Atlas_2019 = pd.read_csv('/Users/daniellerosenthal/Downloads/2019Atlas.csv')
+#Atlas_2015 = pd.read_csv('/Users/daniellerosenthal/Downloads/2015Atlas.csv')
+
+Atlas_Sets = pd.DataFrame()
+
+def import_atlas_data(year):
+    Atlas_Raw = pd.read_csv('/Users/daniellerosenthal/Downloads/{}Atlas.csv'.format(year))
+    Atlas_Filtered = Atlas_Raw[relevant_columns]
+    Atlas_Filtered['YearLabel'] = year
+
+    Atlas_Sets = pd.concat([Atlas_Sets, Atlas_Filtered])
+
+
+def make_request():
+    time.sleep(0.1)
+    resp = requests.get(census_base_call, params=params)
+    return resp
+
+def import_census():
+    response = requests.get(cbc)
+    body = response.headers
+    return response
+    #print(response)
