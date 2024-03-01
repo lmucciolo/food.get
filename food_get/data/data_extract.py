0ce86e41b1b79f307c1140fc4b3cbb22918a4a4a
Danielle Rosenthal <danieller530@gmail.com>
2024-03-01 12:59:11 -0600

    adding large files to gitignore for now

--- food_get/data/data_extract.py
+++ food_get/data/data_extract.py
@@ -13,15 +13,6 @@ Description:
 import pandas as pd
 import numpy as np
 
-relevant_columns = ['CensusTract', 'State', 'County', 'Urban', 'Pop2010', 'OHU2010', 'lapop1',
-'lapop1share', 'lalowi1', 'lalowi1share', 'lasnap1', 'lasnap1share', 'lapop10',
-'lapop10share', 'lalowi10', 'lalowi10share', 'lapop20', 'lapop20share', 'lalowi20',
-'lalowi20share']
-
-Atlas_2019 = pd.read_csv('/Users/daniellerosenthal/Downloads/AtlasData/Atlas2019.csv')
-Atlas_2015 = pd.read_csv('/Users/daniellerosenthal/Downloads/AtlasData/Atlas2015.csv')
-Atlas_2010 = pd.read_csv('/Users/daniellerosenthal/Downloads/AtlasData/Atlas2010.csv')
-Atlas_Sets = pd.DataFrame()
 
 def import_atlas_data(year):
     Atlas_Raw = pd.read_csv('/Users/daniellerosenthal/Downloads/{}Atlas.csv'.format(year))
